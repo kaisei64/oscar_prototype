665ca6ec0e42281bdfc93481ef13f78f6d9a58b6
kaisei <kaise64taka84@keio.jp>
2020-05-15 12:07:04 +0900

    first commit

---
+++ dataset.py
@@ -0,0 +1,69 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+from util_func import split_train_val, batch_elastic_transform
+
+device = 'cuda'
+dtype = torch.float
+
+# criterion = nn.CrossEntropyLoss()
+
+transform_train = transforms.Compose([
+    transforms.ToTensor(),
+])
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+])
+
+train_val_dataset = torchvision.datasets.MNIST(root='./data/',
+                                               train=True,
+                                               transform=transform_train,
+                                               download=True)
+train_dataset, val_dataset = split_train_val(train_val_dataset, 0.8)
+test_dataset = torchvision.datasets.MNIST(root='./data/',
+                                          train=False,
+                                          transform=transform_test,
+                                          download=True)
+
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=256,
+                                           shuffle=True,
+                                           num_workers=2)
+val_loader = torch.utils.data.DataLoader(dataset=val_dataset,
+                                         batch_size=256,
+                                         shuffle=True,
+                                         num_workers=2)
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=256,
+                                          shuffle=False,
+                                          num_workers=2)
+
+# check if the function batch_elastic_transform works
+# if __name__ == "__main__":
+#     import matplotlib.pyplot as plt
+#
+#     '''
+#     the following code demonstrates how gaussian_filter works by ploting
+#     the displacement field before and after applying the gaussian_filter
+#     '''
+#     # random_state = np.random.RandomState(None)
+#     # dx1 = random_state.rand(28, 28) * 2 - 1
+#     # dy1 = random_state.rand(28, 28) * 2 - 1
+#     # dx2 = gaussian_filter(dx1, 4, mode='constant')
+#     # dy2 = gaussian_filter(dy1, 4, mode='constant')
+#     # x, y = np.mgrid[0:28, 0:28]
+#     # plt.quiver(x, y, dx1, dy1)
+#     # plt.show()
+#     # plt.quiver(x, y, dx2, dy2)
+#     # plt.show()
+#
+#     for images, labels in train_loader:
+#         plt.imshow(images[0].reshape(28, -1), cmap='gray')
+#         plt.show()
+#         dimg = batch_elastic_transform(images.reshape(-1, 784), sigma=4, alpha=20, height=28, width=28)
+#         dimg = dimg.reshape(-1, 28, 28)
+#         plt.imshow(dimg[0], cmap='gray')
+#         plt.show()
+#         plt.close()
+#         break
---
+++ util_func.py
@@ -0,0 +1,67 @@
+import torch
+import numpy as np
+from scipy.ndimage.interpolation import map_coordinates
+from scipy.ndimage.filters import gaussian_filter
+
+
+def split_train_val(train_val_dataset, train_rate):
+    n_samples = len(train_val_dataset)
+    train_size = int(len(train_val_dataset) * train_rate)
+    val_size = n_samples - train_size
+    return torch.utils.data.random_split(train_val_dataset, [train_size, val_size])
+
+
+def batch_elastic_transform(images, sigma, alpha, height, width, random_state=None):
+    """
+    this code is borrowed from chsasank on GitHubGist
+    Elastic deformation of images as described in [Simard 2003].
+
+    images: a two-dimensional numpy array; we can think of it as a list of flattened images
+    sigma: the real-valued variance of the gaussian kernel
+    alpha: a real-value that is multiplied onto the displacement fields
+
+    returns: an elastically distorted image of the same shape
+    """
+    assert len(images.shape) == 2
+    # the two lines below ensure we do not alter the array images
+    e_images = np.empty_like(images)
+    e_images[:] = images
+    e_images = e_images.reshape(-1, height, width)
+
+    if random_state is None:
+        random_state = np.random.RandomState(None)
+
+    x, y = np.mgrid[0:height, 0:width]
+    for i in range(e_images.shape[0]):
+        dx = gaussian_filter((random_state.rand(height, width) * 2 - 1), sigma, mode='constant') * alpha
+        dy = gaussian_filter((random_state.rand(height, width) * 2 - 1), sigma, mode='constant') * alpha
+        indices = x + dx, y + dy
+        e_images[i] = map_coordinates(e_images[i], indices, order=1)
+
+    return e_images.reshape(-1, 784)
+
+
+def list_of_distances(X, Y):
+    """
+    Given a list of vectors, X = [x_1, ..., x_n], and another list of vectors,
+    Y = [y_1, ... , y_m], we return a list of vectors
+            [[d(x_1, y_1), d(x_1, y_2), ... , d(x_1, y_m)],
+             ...
+             [d(x_n, y_1), d(x_n, y_2), ... , d(x_n, y_m)]],
+    where the distance metric used is the sqared euclidean distance.
+    The computation is achieved through a clever use of broadcasting.
+    """
+    XX = tf.reshape(list_of_norms(X), shape=(-1, 1))
+    YY = tf.reshape(list_of_norms(Y), shape=(1, -1))
+    output = XX + YY - 2 * tf.matmul(X, tf.transpose(Y))
+
+    return output
+
+
+def list_of_norms(X):
+    """
+    X is a list of vectors X = [x_1, ..., x_n], we return
+        [d(x_1, x_1), d(x_2, x_2), ... , d(x_n, x_n)], where the distance
+    function is the squared euclidean distance.
+    """
+    return tf.reduce_sum(tf.pow(X, 2), axis=1)
