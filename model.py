e2b0934cef9ad30bd2a15b2cea64cea8ffef032b
kaisei <kaise64taka84@keio.jp>
2020-05-17 15:48:58 +0900

    refactoring

--- model.py
+++ model.py
@@ -21,25 +21,25 @@ class ProtoNet(nn.Module):
             nn.ReLU(inplace=True)
         )
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(class_num, 32, kernel_size=3, stride=2, padding=1),
+            nn.ConvTranspose2d(class_num, 32, kernel_size=3, stride=2, padding=1, output_padding=1),
             nn.ReLU(inplace=True),
             nn.ConvTranspose2d(32, 32, kernel_size=3, stride=2, padding=1),
             nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(32, 32, kernel_size=3, stride=2, padding=1),
+            nn.ConvTranspose2d(32, 32, kernel_size=3, stride=2, padding=1, output_padding=1),
             nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(32, 1, kernel_size=3, stride=2, padding=1),
+            nn.ConvTranspose2d(32, 1, kernel_size=3, stride=2, padding=1, output_padding=1),
             nn.Sigmoid()
         )
-        self.prototype_feature_vectors = nn.Parameter(torch.rand((prototype_num, 2*2*class_num), device=device, dtype=dtype))
+        self.prototype_feature_vectors = nn.Parameter(torch.nn.init.uniform_(torch.empty(prototype_num, 2*2*class_num)))
         self.classifier = nn.Sequential(
             nn.Linear(prototype_num, class_num, bias=False)
         )
 
     def forward(self, x):
         feature_vectors = self.encoder(x)
-        ae_output = self.avgpool(feature_vectors)
-        feature_vectors = torch.flatten(feature_vectors)
+        ae_output = self.decoder(feature_vectors)
+        feature_vectors = feature_vectors.reshape(-1, 2 * 2 * class_num)
         prototype_distances = list_of_distances(feature_vectors, self.prototype_feature_vectors)
         feature_vector_distances = list_of_distances(self.prototype_feature_vectors, feature_vectors)
-        output = self.classifier(feature_vectors)
-        return ae_output, prototype_distances, feature_vector_distances, F.softmax(output), output
+        output = self.classifier(prototype_distances)
+        return ae_output, prototype_distances, feature_vector_distances, output, F.softmax(output)
