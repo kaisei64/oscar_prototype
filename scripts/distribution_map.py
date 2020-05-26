70ae586c4dc967d1828c36058103e6f8c3c9de05
kaisei <kaise64taka84@keio.jp>
2020-05-26 12:03:53 +0900

    add prototype plot

--- scripts/distribution_map.py
+++ scripts/distribution_map.py
@@ -10,26 +10,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 
-prototype = "15"
-train_net = parameter_use(f'./result/pkl/prototype_{prototype}/train_model_1500.pkl')
-# train_net = parameter_use(f'./result/pkl/prototype_{prototype}/train_model_epoch1500_{prototype}.pkl')
+prototype = "5"
+# train_net = parameter_use(f'./result/pkl/prototype_{prototype}/train_model_1500.pkl')
+train_net = parameter_use(f'./result/pkl/prototype_{prototype}/train_model_epoch1500_{prototype}.pkl')
 
 examples_to_show = 10000
 examples = [train_dataset[i][0] for i in range(examples_to_show)]
 examples = torch.cat(examples).reshape(len(examples), *examples[0].shape).to(device)
 examples_labels = [train_dataset[i][1] for i in range(examples_to_show)]
 color_set = ["b", "g", "r", "c", "m", "y", "k", '#f781bf', '#a65628', '#ff7f00']
+examples_labels = np.where(examples_labels == "0", "b", examples_labels)
 for i in range(model.class_num):
     examples_labels = np.where(examples_labels == str(i), color_set[i], examples_labels)
 feature_vec = train_net.encoder(examples).reshape(-1, model.class_num*2*2).cpu().detach().numpy()
+prototype_feature_vec = train_net.prototype_feature_vectors.reshape(-1, model.class_num*2*2)\
+                        .cpu().detach().numpy()
+con_vec = np.concatenate([feature_vec, prototype_feature_vec])
+
 pca = PCA(n_components=2)
-pca.fit(feature_vec)
-data_pca = pca.transform(feature_vec)
+pca.fit(con_vec)
+data_pca = pca.transform(con_vec)
 fig = plt.figure(figsize=(15, 12), facecolor='w')
 plt.rcParams["font.size"] = 15
-for i in range(examples_to_show):
+num = [i for i in range(model.class_num)]
+j = 0
+for i in range(examples_to_show + int(prototype)):
     # plot data
-    plt.plot(data_pca[i][0], data_pca[i][1], ms=5.0, zorder=2, marker="x", color=examples_labels[i])
-    # show label
-    # plt.annotate(words[i][0], (data_pca[i][0], data_pca[i][1]), size=12)
+    if i < examples_to_show:
+        plt.plot(data_pca[i][0], data_pca[i][1], ms=5.0, zorder=2, marker="x", color=examples_labels[i])
+    # plot prototype
+    else:
+        plt.plot(data_pca[i][0], data_pca[i][1], ms=50.0, zorder=2, marker=".", color='gray')
+        # show prototype order
+        plt.annotate(num[j], (data_pca[i][0], data_pca[i][1]), size=30)
+        j += 1
 plt.show()
