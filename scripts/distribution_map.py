8c8b7131cf6b1b9d8206a8886c5d381d1f715074
kaisei <kaise64taka84@keio.jp>
2020-05-27 20:41:50 +0900

    show legend in the distribution_map

--- scripts/distribution_map.py
+++ scripts/distribution_map.py
@@ -19,6 +19,7 @@ examples_to_show = 10000
 examples = [train_dataset[i][0] for i in range(examples_to_show)]
 examples = torch.cat(examples).reshape(len(examples), *examples[0].shape).to(device)
 examples_labels = [train_dataset[i][1] for i in range(examples_to_show)]
+examples_class_labels = examples_labels.copy()
 color_set = ["b", "g", "r", "c", "m", "y", "k", '#f781bf', '#a65628', '#ff7f00']
 examples_labels = np.where(examples_labels == "0", "b", examples_labels)
 for i in range(model.class_num):
@@ -35,14 +36,25 @@ fig = plt.figure(figsize=(15, 12), facecolor='w')
 plt.rcParams["font.size"] = 15
 num = [i for i in range(int(prototype))]
 j = 0
+handles, label_keep = [], []
 for i in range(examples_to_show + int(prototype)):
     # plot data
     if i < examples_to_show:
-        plt.plot(data_pca[i][0], data_pca[i][1], ms=5.0, zorder=2, marker="x", color=examples_labels[i])
+        line, = plt.plot(data_pca[i][0], data_pca[i][1], ms=5.0, zorder=2, marker="x",
+                         color=examples_labels[i], label=examples_class_labels[i])
+        if not(examples_labels[i] in label_keep):
+            label_keep.append(examples_labels[i])
+            handles.append(line)
     # plot prototype
     else:
         plt.plot(data_pca[i][0], data_pca[i][1], ms=50.0, zorder=2, marker=".", color='gray')
         # show prototype order
-        plt.annotate(num[j], (data_pca[i][0], data_pca[i][1]), size=50)
+        plt.annotate(num[j], (data_pca[i][0], data_pca[i][1]), size=30)
         j += 1
+legend_order = [color_set.index(val) for val in label_keep]
+handles_order = [legend_order.index(val) for val in range(model.class_num)]
+sorted_handles = [handles[idx] for idx in handles_order]
+fig.legend(loc='upper right', fontsize=25, handles=sorted_handles)
 plt.savefig(f'./result/png/prototype_{prototype}/distribution_map.png')
+# plt.savefig(f'./result/png/prototype_{prototype}_sub/distribution_map.png')
+# plt.show()
