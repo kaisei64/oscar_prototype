d14e5d3b613a72f60e43da11b479b83d20260815
kaisei <kaise64taka84@keio.jp>
2020-06-05 14:35:18 +0900

    change the color of the column in the table

--- scripts/classifier_weight_vis.py
+++ scripts/classifier_weight_vis.py
@@ -5,9 +5,9 @@ sys.path.append(pardir)
 from util_func import parameter_use
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
-prototype = "4"
-# train_net = parameter_use(f'./result/pkl/prototype_{prototype}/train_model_1500.pkl')
+prototype = "15"
 train_net = parameter_use(f'./result/pkl/prototype_{prototype}/train_model_epoch500_{prototype}.pkl')
 classifier_weight = train_net.classifier[0].weight.cpu().detach().numpy().T
 
@@ -17,14 +17,18 @@ df = pd.DataFrame(classifier_weight, columns=class_name)
 # print(df)
 fig, ax = plt.subplots(figsize=((len(df.columns))*1.2, (len(df))*0.4))
 ax.axis('off')
-colors = [["silver" if classifier_weight[j][i] - min(classifier_weight[j]) < abs(min(classifier_weight[j])) * 0.4
-           else "w" for i in range(len(classifier_weight[0]))]
-          for j in range(len(classifier_weight[:, 0]))]
+# Color the weights that contribute a lot
+# colors = [["silver" if classifier_weight[j][i] - min(classifier_weight[j]) < abs(min(classifier_weight[j])) * 0.4
+#            else "w" for i in range(len(classifier_weight[0]))]
+#           for j in range(len(classifier_weight[:, 0]))]
+vals = np.around(df.values, 2)
+normal = plt.Normalize(vals.min()-1, vals.max()+1)
 tbl = ax.table(cellText=df.values.round(3),
                bbox=[0.05, 0, 0.9, 1],
                colLabels=df.columns,
                rowLabels=prototype_name,
-               cellColours=colors)
+               # cellColours=colors)
+               cellColours=plt.cm.gray(normal(vals)))
 tbl.set_fontsize(30)
 plt.savefig(f'./result/png/prototype_{prototype}/classifier_weight.png')
 # plt.show()
