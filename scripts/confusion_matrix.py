36999f74fa828dc36aef49b3aaaf48373202f59a
kaisei <kaise64taka84@keio.jp>
2020-08-21 10:29:01 +0900

    add test_acc_vis func

--- scripts/confusion_matrix.py
+++ scripts/confusion_matrix.py
@@ -2,7 +2,7 @@ import os
 import sys
 pardir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(pardir)
-from util_func import parameter_use, make_confusion_matrix
+from util_func import parameter_use, make_confusion_matrix, pruningtestacc_vis
 from dataset import *
 import torch
 
@@ -14,8 +14,8 @@ save_step = 10
 sigma = 4
 alpha = 20
 
-for k in range(1, 11):
-    train_net = parameter_use(f'./result/pkl/prototype_{prototype}/prune_dense/abs/prune_proto_finetune_from_abs_small/'
+for k in range(2, 11):
+    train_net = parameter_use(f'./result/pkl/prototype_{prototype}/prune_dense/not_abs/prune_conv_finetune_from_small/'
                               f'prune_train_model_epoch{k}_{prototype}.pkl')
     output_hold = torch.empty(0)
     label_hold = torch.empty(0)
@@ -34,6 +34,22 @@ for k in range(1, 11):
         avg_test_acc = test_acc / len(test_loader.dataset)
         print(f'test_acc: {avg_test_acc:.4f}')
 
-    make_confusion_matrix(f'./result/png/prototype_{prototype}/prune_finetune/abs/prune{k}_confusion_matrix.png',
-                          f'./result/png/prototype_{prototype}/prune_finetune/abs/prune{k}_classification_report.png',
+    make_confusion_matrix(f'./result/png/prototype_{prototype}/prune_finetune/not_abs/prune{k}_confusion_matrix.png',
+                          f'./result/png/prototype_{prototype}/prune_finetune/not_abs/prune{k}_classification_report.png',
                           label_hold, output_hold)
+
+# test_acc_vis
+# learning_history = {'test_acc': []}
+# for k in range(1, 11):
+#     train_net = parameter_use(f'./result/pkl/prototype_{prototype}/prune_dense/abs/prune_proto_finetune_from_abs_small/'
+#                               f'prune_train_model_epoch{k}_{prototype}.pkl')
+#     with torch.no_grad():
+#         avg_test_acc, test_acc = 0, 0
+#         for images, labels in test_loader:
+#             images, labels = images.to(device), labels.to(device)
+#             ae_output, prototype_distances, feature_vector_distances, outputs, softmax_output = train_net(
+#                 images)
+#             test_acc += (outputs.max(1)[1] == labels).sum().item()
+#         avg_test_acc = test_acc / len(test_loader.dataset)
+#         learning_history['test_acc'].append(avg_test_acc)
+# pruningtestacc_vis(f'./result/png/prototype_{prototype}/testacc_afterprune.png', 10, learning_history)
