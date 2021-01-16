1a1fd0be4dbdcefee7eb511f8d11952a137ffaad
kaisei <kaise64taka84@keio.jp>
2021-01-16 14:51:46 +0900

    move 2 func from util_func.py

--- dataset.py
+++ dataset.py
@@ -2,13 +2,55 @@ import torch
 import torchvision
 import torch.nn as nn
 import torchvision.transforms as transforms
-from util_func import split_train_val, batch_elastic_transform
+import numpy as np
+from scipy.ndimage.interpolation import map_coordinates
+from scipy.ndimage.filters import gaussian_filter
+# from util_func import split_train_val, batch_elastic_transform
 
 device = 'cuda'
 dtype = torch.float
 criterion = nn.CrossEntropyLoss()
 batch_size = 250
 in_height, in_width = 28, 28
+verbose = True
+
+
+def split_train_val(dataset, train_rate):
+    n_samples = len(dataset)
+    train_size = int(len(dataset) * train_rate)
+    val_size = n_samples - train_size
+    return torch.utils.data.random_split(dataset, [train_size, val_size])
+
+
+def batch_elastic_transform(images, sigma, alpha, height, width, random_state=None):
+    """
+    this code is borrowed from chsasank on GitHubGist
+    Elastic deformation of images as described in [Simard 2003].
+
+    images: a two-dimensional numpy array; we can think of it as a list of flattened images
+    sigma: the real-valued variance of the gaussian kernel
+    alpha: a real-value that is multiplied onto the displacement fields
+
+    returns: an elastically distorted image of the same shape
+    """
+    # import dataset
+    assert len(images.shape) == 2
+    # the two lines below ensure we do not alter the array images
+    e_images = np.empty_like(images)
+    e_images[:] = images
+    e_images = e_images.reshape(-1, height, width)
+
+    if random_state is None:
+        random_state = np.random.RandomState(None)
+
+    x, y = np.mgrid[0:height, 0:width]
+    for i in range(e_images.shape[0]):
+        dx = gaussian_filter((random_state.rand(height, width) * 2 - 1), sigma, mode='constant') * alpha
+        dy = gaussian_filter((random_state.rand(height, width) * 2 - 1), sigma, mode='constant') * alpha
+        indices = x + dx, y + dy
+        e_images[i] = map_coordinates(e_images[i], indices, order=1)
+    return e_images.reshape(-1, in_height*in_width)
+
 
 transform_train = transforms.Compose([
     # transforms.Grayscale(num_output_channels=1),  # cifar10
@@ -29,6 +71,9 @@ train_val_dataset = torchvision.datasets.MNIST(root='./data/',
                                                  transform=transform_train,
                                                  download=True)
 train_dataset, val_dataset = split_train_val(train_val_dataset, 0.8)
+# print(train_dataset.dataset)
+# print(train_dataset.indices.shape)
+# exit()
 test_dataset = torchvision.datasets.MNIST(root='./data/',
 # test_dataset = torchvision.datasets.FashionMNIST(root='./data/',
 # test_dataset = torchvision.datasets.CIFAR10(root='./data/',
@@ -36,6 +81,10 @@ test_dataset = torchvision.datasets.MNIST(root='./data/',
                                             transform=transform_test,
                                             download=True)
 
+train_val_loader = torch.utils.data.DataLoader(dataset=train_val_dataset,
+                                               batch_size=batch_size,
+                                               shuffle=True,
+                                               num_workers=2)
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
                                            batch_size=batch_size,
                                            shuffle=True,
